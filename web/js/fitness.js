3af4fbde9e129a242e0539594ad2bad2bffd1711
lakshaytyagi111 <lakshayindia420@gmail.com>
2024-05-25 15:25:48 +0530

    date feature in fitness tracker fixed

--- web/js/fitness.js
+++ web/js/fitness.js
@@ -13,7 +13,7 @@ class WorkoutTracker {
             const date = new Date();
             const year = date.getFullYear();
             const month = (date.getMonth() + 1).toString().padStart(2, "0");
-            const day = date.getDay().toString().padStart(2, "0");
+            const day = date.getDate().toString().padStart(2, "0");
 
             this.addEntry({
                 date: `${ year }-${ month }-${ day }`,
